4b4b09a11f3a92ec322995e1dcbd7b1c498599b3
Cola <bcad1591@gmail.com>
2022-05-29 21:48:25 +0900

    refactor: 불필요한 import 제거

--- src/lib/styles/icons.tsx
+++ src/lib/styles/icons.tsx
@@ -1,9 +1,3 @@
-import styled from 'styled-components';
-
-const Common = styled.i`
-  font-size: inherit;
-`;
-
 export function Prev() {
   return (
     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
